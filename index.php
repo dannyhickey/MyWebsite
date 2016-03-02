ec02e3c5fb6aeb9f165295f2e33fc2fe63898b50
Daniel Hickey <daniellhickey@gmail.com>
2016-03-02 16:25:13 +0000

    Styling the navigation of the index page with bootstrap

---
+++ index.php
@@ -0,0 +1,55 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>		
+				<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		
+		<!--jQuery CSS-->
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+		
+		<!--FontAwesome-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+		<!--jQuery-->
+		<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+
+		<!--jQuery UI-->		
+		<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+		
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">About Me</a></li>
+					<li><a href="#">FAQ</a></li>
+					<li><a href="#">Contact</a></li>				
+				</ul>
+			</div>			
+		</nav><!-- END of main NAV-->
+		
+		<div class="container">
+			
+			<h1>Content</h1>
+			
+		</div>
+		
+		<footer id="footer">
+			<div class="container">
+				<p>insert footer here</p>
+				
+			</div>
+			
+		</footer>
+		
+				
+	</body>
+		
+	
+</html>
