a4e95d088bd1ecbcfd092140c733a2d0fd54ad41
Daniel Hickey <daniellhickey@gmail.com>
2016-03-02 16:57:30 +0000

    Commmenting and cleaning code wrapping the footer to bottom

--- index.php
+++ index.php
@@ -1,28 +1,51 @@
 ﻿<!DOCTYPE html>
 <html>
-	<head>		
-				<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-		
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-		
-		<!--jQuery CSS-->
-		<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
-		
-		<!--FontAwesome-->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<head>		
+			<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	
+	<!--jQuery CSS-->
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+	
+	<!--FontAwesome-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+			
+	<!--jQuery-->
+	<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 
-		<!--jQuery-->
-		<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-
-		<!--jQuery UI-->		
-		<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+	<!--jQuery UI-->		
+	<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+	
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	
+	<style>
+		html,body
+		{
+			height: 100%;	
+		}
 		
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-	</head>
-	<body>
+		#wrap
+		{
+			min-height: 100%;
+			height: auto;
+			margin: 0 auto -60px;
+			padding: 0 0 60px;
+		}
+		
+		#footer 
+		{
+			height: 60px;
+			background-color: #f5f5f5;
+		}
+	</style>
+
+</head>
+<body>
+	<div id="wrap">
 		<nav class="navbar navbar-default" role="navigation">
 			<div class="container">
 				<ul class="nav navbar-nav">
@@ -34,22 +57,17 @@
 			</div>			
 		</nav><!-- END of main NAV-->
 		
-		<div class="container">
-			
-			<h1>Content</h1>
-			
+		<div class="container">				
+			<h1>Content</h1>				
 		</div>
-		
-		<footer id="footer">
-			<div class="container">
-				<p>insert footer here</p>
-				
-			</div>
-			
-		</footer>
-		
-				
-	</body>
-		
+	</div><!--END wrapper-->
+	
+	<footer id="footer">
+		<div class="container">
+			<p>insert footer here</p>				
+		</div>			
+	</footer>	<!--END footer-->				
+</body>
+	
 	
 </html>
