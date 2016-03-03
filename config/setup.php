70ade8e9c55a4c24f17c19152e3dc73f692f09c7
Daniel Hickey <daniellhickey@gmail.com>
2016-03-03 09:49:19 +0000

    Error handling in the setup file

--- config/setup.php
+++ config/setup.php
@@ -7,7 +7,7 @@ $password = 'ZQEGAF3h4';
 $database = 'danielhickey_db';
 
 # Database Connection Here...
-$dbc = mysqli_connect($host, $user, $password, $database);
+$dbc = mysqli_connect($host, $user, $password, $database) OR die('Unable to connect because: '.mysqli_connect_error());// die function to kill the page if something is wrong
 
 $siteTitle = 'Daniel Hickey';
 $pageTitle = 'Home Page';
