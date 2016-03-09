3fa88fd70f6fb30cb2fbdd09d5cec84642c54858
Daniel Hickey <daniellhickey@gmail.com>
2016-03-09 09:30:25 +0000

    Edited variables due to causing unusual behaviour when saving form

--- DanielHickey/config/connection.php
+++ DanielHickey/config/connection.php
@@ -1,12 +1,6 @@
 <?php
 
-$host = 'localhost';
-$user = 'dev'; 
-$password = 'ZQEGAF3h4';
-$database = 'danielhickey_db';
-
-
 # Database Connection Here...
-$dbc = mysqli_connect($host, $user, $password, $database) OR die('Unable to connect because: '.mysqli_connect_error());// die function to kill the page if something is wrong
+$dbc = mysqli_connect('localhost', 'dev', 'ZQEGAF3h4', 'danielhickey_db') OR die('Unable to connect because: '.mysqli_connect_error());// die function to kill the page if something is wrong
 
 ?>
