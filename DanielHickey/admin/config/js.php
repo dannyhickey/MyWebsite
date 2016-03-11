e4b7dd131fa2d4dc0d225af4b07c3a78a871a15d
Daniel Hickey <daniellhickey@gmail.com>
2016-03-11 10:03:00 +0000

    Added TinyMCE WYSIWIG to a new folder called js in the admin folder. TinyMCE setup is located in the js.php file on the admin side

--- DanielHickey/admin/config/js.php
+++ DanielHickey/admin/config/js.php
@@ -1,24 +1,33 @@
 <?php
 // JavaScript file:
 ?>
-	
+
 <!--jQuery-->
 <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 
-<!--jQuery UI-->		
+<!--jQuery UI-->
 <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
+<script src='//cdn.tinymce.com/4/tinymce.min.js'></script>
+
+<!--
+<script src="js/tinymce/tinymce.min.js"></script>
+-->
+
 <script>
-	
-	$(document).ready(function(){
+	$(document).ready(function() {
 		$("#console_debug").hide();
-		
-		$("#btn_debug").click(function(){
+
+		$("#btn_debug").click(function() {
 			$("#console_debug").toggle();
 		});
 	});
+
+	tinymce.init({
+    selector: 'textarea'
+  });
 	
 </script>
