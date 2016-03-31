102129e289b73573b78a6b12af4b33b4b91dbec7
Daniel Hickey <daniellhickey@gmail.com>
2016-03-31 19:57:00 +0100

    getting rid of warnings

--- DanielHickey/config/connection.php
+++ DanielHickey/config/connection.php
@@ -7,6 +7,6 @@ DEFINE ('DB_NAME', 'danielhickey_db');
 
 
 # Database Connection Here...
-$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect because: '.mysqli_connect_error());
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect because: '.mysqli_connect_error());
 
 ?>
